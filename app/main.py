4c7090e56d2effcad3ee692390e5e06cbc0a4e2d
Leo <leonardo.pol@capgemini.com>
2022-08-29 13:24:48 +0200

    add cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .routers import posts, users, auth
@@ -10,6 +11,14 @@ models.Base.metadata.create_all(bind=engine)
 # docker run --name postgres-container -e POSTGRES_USER=leo -e POSTGRES_PASSWORD=psw -p 5432:5432 -v /data:/var/lib/postgresql/data -d postgres
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="http:localhost:3000",
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(posts.posts_router)
 app.include_router(users.users_router)
 app.include_router(auth.auth_router)
